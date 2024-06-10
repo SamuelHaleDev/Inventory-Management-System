41a96fce28097217a06a7a03d516e9c2704ac1c0
SamuelHaleDev <samuelhalebusiness@gmail.com>
2024-06-10 12:38:40 -0400

    Created Item Class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,47 @@
 ﻿namespace Inventory_Management_System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+// Item Class
+class Item {
+    private string name;
+    private int quantity;
+    private float price;
+
+    public Item(string n, int q, float p) {
+      name = n;
+      quantity = q;
+      price = p;
     }
+
+    public string GetName() {
+      return name;
+    }
+
+    public int GetQuantity() {
+      return quantity;
+    }
+
+    public float GetPrice() {
+      return price;
+    }
+
+    public void SetName(string n) {
+      name = n;
+    }
+
+    public void SetQuantity(int q) {
+      quantity = q;
+    }
+
+    public void SetPrice(float p) {
+      price = p;
+    }
+}
+
+// Inventory Class
+
+
+class Program {
+  static void Main(string[] args) {
+    
+  }
 }
